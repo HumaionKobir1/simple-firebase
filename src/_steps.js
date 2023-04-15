1fc147e7e0cd9d8c617c5de55aceec09509e390f
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-15 11:07:25 +0600

    Create firebase project and install firebase SDK

---
+++ src/_steps.js
@@ -0,0 +1,9 @@
+/**
+ * . visit: console.firebase.google.com
+ * 2. Create project (skip google analytics)
+ * 3. Register app (create config)
+ * 4. install firebase: npm install firebase
+ * 5. add config file to your project
+ * 6. DANGER: Do not publish or make firebase config to public by pushing those to github;
+ * 
+ * */ 
